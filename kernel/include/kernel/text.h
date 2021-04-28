f74c427708090518a1182ea0c25c5c02652b9ab8
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-28 09:44:20 -0700

    Create text.h

---
+++ kernel/include/kernel/text.h
@@ -0,0 +1,11 @@
+#ifndef _KERNEL_TEXT_H
+#define _KERNEL_TEXT_H
+ 
+#include <stddef.h>
+ 
+void terminal_initialize(void);
+void putchar(char c);
+void write(const char* data, size_t size);
+void writestring(const char* data);
+ 
+#endif
